88dc67d7d126b7f311835ce868f733020c381144
Philip Mooney <C14485132@mydit.ie>
2016-11-14 14:56:05 +0000

    Beginning the event programming (crashes on launch after adding event programming, fix later)

--- app/src/main/java/com/example/phili/rifletracker/MainActivity.java
+++ app/src/main/java/com/example/phili/rifletracker/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.phili.rifletracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnRecentEvents = (Button)findViewById(R.id.buttonRecentEvents);
+    Button btnAddNewEvent;
+    Button btnExportEvents;
+    Button btnSettings;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test2
+
+        btnRecentEvents.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(getApplicationContext(),RecentEvents.class);
+                startActivity(i);
+            }
+        });
     }
+
+
 }
