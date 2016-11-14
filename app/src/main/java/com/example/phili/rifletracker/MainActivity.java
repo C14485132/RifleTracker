64f67096bb42850d1a03861c2cc9a3e3c03cd243
Philip Mooney <C14485132@mydit.ie>
2016-11-14 15:14:54 +0000

    Buttons working on main screen

--- app/src/main/java/com/example/phili/rifletracker/MainActivity.java
+++ app/src/main/java/com/example/phili/rifletracker/MainActivity.java
@@ -8,27 +8,60 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnRecentEvents = (Button)findViewById(R.id.buttonRecentEvents);
-    Button btnAddNewEvent;
-    Button btnExportEvents;
-    Button btnSettings;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btnRecentEvents = (Button)findViewById(R.id.buttonRecentEvents);
+        Button btnAddNewEvent = (Button)findViewById(R.id.buttonAddNewEvent);
+        Button btnExportEvent =  (Button)findViewById(R.id.buttonExportEvent);
+        Button btnSettings =  (Button)findViewById(R.id.buttonSettings);
+
         btnRecentEvents.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
                 Intent i = new Intent(getApplicationContext(),RecentEvents.class);
                 startActivity(i);
             }
         });
+
+        btnAddNewEvent.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),AddNewEvent.class);
+                startActivity(i);
+            }
+        });
+
+        /* TODO: Export screens
+        btnExportEvent.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),ExportEvent.class);
+                startActivity(i);
+            }
+        });
+        */
+
+        /*TODO: Settings screen
+        btnAddNewEvent.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Settings.class);
+                startActivity(i);
+            }
+        });
+        */
+
+
     }
+
 
 
 }
