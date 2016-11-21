682747a43862dad0842a995b3111b8800752abad
Philip Mooney <C14485132@mydit.ie>
2016-11-21 19:32:29 +0000

    Some settings done, a lot not working well, but with the settings commited this time

---
+++ app/src/main/java/com/example/phili/rifletracker/Settings.java
@@ -0,0 +1,32 @@
+package com.example.phili.rifletracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class Settings extends AppCompatActivity {
+
+    Switch switchDarkMode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(android.R.style.ThemeOverlay_Material);
+        setContentView(R.layout.activity_settings);
+
+        switchDarkMode = (Switch)findViewById(R.id.switchDarkMode);
+
+        switchDarkMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    setTheme(android.R.style.ThemeOverlay_Material_Dark);
+                    setContentView(R.layout.activity_settings);
+                } else {
+                    setTheme(android.R.style.Theme_Material);
+                    setContentView(R.layout.activity_settings);
+                }
+            }
+        });
+    }
+}
